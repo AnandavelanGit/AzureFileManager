4cd78d4402066aaca7646d25781160012051f5cc
Anandavelan Giriloganathan <1000025760@hexaware.com>
2024-02-06 15:02:38 +0530

    msal changes to include scope

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiUrl: 'https://filemanageapi.azurewebsites.net/',
     clientId: 'e1bdbd3a-22f7-47f7-aaea-19bc78e57d0c',
     authority: 'https://login.microsoftonline.com/e3978dc2-ccb8-49b2-8c6e-e9a8d4cf7b38',
-    redirectUri: 'https://red-meadow-0d468f610.4.azurestaticapps.net/',
+    redirectUri: 'https://red-meadow-0d468f610.4.azurestaticapps.net',
     expose: 'api://db7857f4-4929-4c5e-95cb-5c352f65cf46/user_impersonation',
     graphApi: 'https://graph.microsoft.com/v1.0/me'
 
