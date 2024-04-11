13d0caf6cd55201e61ef63f7bf013ef05bacabf4
Anandavelan Giriloganathan <1000025760@hexaware.com>
2024-04-11 13:07:44 +0530

    restriction of write permission

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,15 @@
 export const environment = {
-    defaultComponent:"SecretQuestion2Component",
     production: false,
-
-   apiUrl: 'https://filemanageapi.azurewebsites.net/'
-    //apiUrl: 'https://filemanageapi.azurewebsites.net/',
+    apiUrl: 'https://localhost:5001/',
     clientId: 'e1bdbd3a-22f7-47f7-aaea-19bc78e57d0c',
     authority: 'https://login.microsoftonline.com/e3978dc2-ccb8-49b2-8c6e-e9a8d4cf7b38',
     redirectUri: 'http://localhost:4200/',
-    expose: 'api://db7857f4-4929-4c5e-95cb-5c352f65cf46/user_impersonation',
+    //Enable below for Read permission alone - Admin consent is not required as the scope
+    // is pre-authorized in the api
+    expose: 'api://db7857f4-4929-4c5e-95cb-5c352f65cf46/FileAPIRead',
+
+   //Enable below for ReadWrite permission - Admin consent already provided
+   // expose: 'api://db7857f4-4929-4c5e-95cb-5c352f65cf46/FileAPIReadWrite',
+
     graphApi: 'https://graph.microsoft.com/v1.0/me'
-
-
 };
